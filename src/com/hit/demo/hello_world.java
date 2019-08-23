83de169d307163915d3e9b8e005c1253a8f53839
Dentioner <we980127@gmail.com>
2019-08-23 11:24:04 +0800

    update
    first code of java

---
+++ src/com/hit/demo/hello_world.java
@@ -0,0 +1,8 @@
+package com.hit.demo;
+
+public class hello_world {
+    public static void main (String[] args){
+        System.out.println("I Rule!");
+        System.out.println("The World");
+    }
+}
