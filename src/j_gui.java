299a702989cc0974f042ba565c70c1295fb52ff5
Dentioner <we980127@gmail.com>
2019-08-27 11:22:34 +0800

    update
    until p.386

---
+++ src/j_gui.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class j_gui
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("click me");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(button);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+        frame.getGraphics().fillOval(70,70,100,100);
+        MyDrawPanel mdp = new MyDrawPanel();
+
+    }
+}
+
+class MyDrawPanel extends JPanel
+{
+    public void paintComponent(Graphics g)
+    {
+        Graphics2D g2d = (Graphics2D)g;
+        int red = (int) (Math.random()*255);
+        int green = (int) (Math.random()*255);
+        int blue = (int) (Math.random() * 255);
+        Color startColor = new Color(red, green, blue);
+
+        red = (int) (Math.random()*255);
+        green = (int) (Math.random()*255);
+        blue = (int) (Math.random() * 255);
+        Color endColor = new Color(red, green, blue);
+
+        GradientPaint gradient = new GradientPaint(70, 70, startColor, 150, 150, endColor);
+        g2d.setPaint(gradient);
+        g2d.fillOval(70,70,100,100);
+
+
+    }
+}
